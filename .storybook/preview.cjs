c71a9216952dc51f512e071cc44c71bb403fb408
Sergio <sergioulloa@protonmail.com>
2022-09-06 10:54:40 -0300

    Who uses a red background?

--- .storybook/preview.cjs
+++ .storybook/preview.cjs
@@ -7,7 +7,7 @@ export const parameters = {
     // Override the default dark theme
     dark: { ...themes.dark, appBg: 'black' },
     // Override the default light theme
-    light: { ...themes.normal, appBg: 'red' },
+    light: { ...themes.normal, appBg: 'white' },
   },
   controls: {
     matchers: {
